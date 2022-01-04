8eabb202eabac9f0b22db85f0376a03a607c6147
ysa5347 <ysa53471@gm.gist.ac.kr>
2022-01-04 14:08:34 +0900

    User, Post, User_Post_match 모델 추가

--- project/backend/post/admin.py
+++ project/backend/post/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, User, User_Post_match
 
 admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(User_Post_match)
+
 # Register your models here.
