d198cc33af30ee8a2d5e5f1baff5768e1f9efc7f
ysa5347 <ysa5347@gmail.com>
2022-02-15 10:58:37 +0900

    FEAT: User-Article-Time_table 참조 관계 설정

--- project/backend/article/models.py
+++ project/backend/article/models.py
@@ -2,9 +2,15 @@
 from django.db import models
 from account.models import CustomUser
 
+class Tag(models.Model):
+    subject = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.subject}'
+
 class Article(models.Model):
     writerID = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL) 
-    lab = models.CharField(max_length=40) # lab name를 어떻게 처리할 건가요?(예를 들어, 우리가 Lab 이름들을 모두 저장해 놓고, 번호로 지정해줄 것인가요 아니면 자유롭게 지정할 것인가용?)
+    lab = models.CharField(max_length=40) 
     title = models.TextField()
     postDate = models.DateField(auto_now_add=True)
     modifiedDate = models.DateField(auto_now=True)
@@ -16,7 +22,7 @@ class Article(models.Model):
     isOffline = models.BooleanField()
     reward = models.PositiveSmallIntegerField() #단위는 원
     location = models.CharField(max_length=30, blank=True, null=True)
-    subject = models.ForeignKey("Tag", max_length=30, blank=True, null=True, on_delete=models.SET_NULL)
+    subject = models.ForeignKey(Tag, max_length=30, blank=True, null=True, on_delete=models.SET_NULL)
     content = models.TextField(max_length = 10000)
     articleFile = models.FileField(blank=True, null=True)
     articleImg = models.ImageField(blank=True, null=True)
@@ -25,22 +31,11 @@ class Article(models.Model):
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.writerID}'
 
-class Tag(models.Model):
-    subject = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return f'{self.subject}'
 
-
-class User_Article_match(models.Model):
-    pkUser = models.PositiveIntegerField()
-    pkPost = models.PositiveIntegerField()
-    timeLength = models.PositiveIntegerField()
 
 class Time_table(models.Model):
-    expInfo = models.PositiveIntegerField()
-    pkUser = models.PositiveIntegerField()
-    startTime = models.PositiveSmallIntegerField()# 0 ~ 1440 min
-    endTime = models.PositiveSmallIntegerField() # 0 ~ 1440 min
-    isCheck = models.BooleanField(default=False) # True: 예약이 차 있다.  
-    day = models.DateField()
+    pk = models.PositiveIntegerField(primary_key=True)
+    article = models.ForeignKey(Article)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    ptcpUser = models.ForeignKey(CustomUser)
