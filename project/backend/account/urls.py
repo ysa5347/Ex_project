e1908a9a7aab368c7470c9af54c411378e89808c
ysa5347 <ysa5347@gmail.com>
2022-02-16 14:26:54 +0900

    FEAT: login API prototype, create API pk 입력 제거

--- project/backend/account/urls.py
+++ project/backend/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.urls.resolvers import URLPattern
 from . import views
-from .views import IsIdValid
+from .views import IsIdValid, loginUser, logoutUser, viewsession
 
 urlpatterns = [
     path("signup/", views.createUser),
     path("isidvalid/", IsIdValid),
-    
+    path("login/", loginUser),
+    path("logout/", logoutUser),
+    path("view/", viewsession),
 ]
