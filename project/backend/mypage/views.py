92e85b4ac417c5d9892e28068e488985633bf7e0
ysa5347 <ysa5347@gmail.com>
2022-04-04 21:45:47 +0900

    FEAT: getUserPtcp 제작 중 막힘(1)

--- project/backend/mypage/views.py
+++ project/backend/mypage/views.py
@@ -4,10 +4,11 @@ from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import getUserSerializer, getUserArticleSerializer
+
+from .serializers import getUserSerializer, getUserArticleSerializer,getUserPtcpSerializer, getUserPtcpTimeSerializer
 import time
 from account.models import CustomUser
-from article.models import Article
+from article.models import Article, TimeTable, UserTimeMatchTable
 
 @api_view(['GET'])
 def getUser(request):
@@ -33,4 +34,11 @@ def getUserArticle(request):
 
 @api_view(['GET'])
 def getUserPtcp(request):
-    pass
+    if not request.session.session_key:
+        return Response('로그인이 필요한 요청입니다.', status=status.HTTP_403_FORBIDDEN)
+    userID = request.user
+    loginUser = CustomUser.objects.get(userID=userID)
+    matchtableData = UserTimeMatchTable.objects.filter(userID=userID)
+    # timetableData = TimeTable.objects.filter(ptcpUser=userID)
+    serializer = getUserPtcpSerializer(matchtableData, many=True)
+    return Response(serializer.data)
