1a67391d7c386c7de89e426856694fb629b43363
ysa5347 <ysa5347@gmail.com>
2022-10-12 16:19:46 +0900

    HOT: named urls

--- project/backend/mypage/urls.py
+++ project/backend/mypage/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import getUser, getUserArticle, getUserPtcp
 
 urlpatterns =[
-    path("getuser/", getUser),
-    path("getarticle/", getUserArticle),
-    path("getuserptcp/", getUserPtcp),
+    path("getuser/", getUser, name='getuser'),
+    path("getarticle/", getUserArticle, name='getarticle'),
+    path("getuserptcp/", getUserPtcp, name='getuserptcp'),
 ]
