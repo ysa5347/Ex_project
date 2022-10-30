1df66e325d1e16874c61210af1fd6515e24da754
ysa5347 <ysa5347@gmail.com>
2022-10-30 13:40:42 +0900

    FEAT: server.sh 내 if문 제거

--- server.sh
+++ server.sh
@@ -2,7 +2,4 @@
 cd /Ex_finder_server/Ex_project/project/backend
 python3 manage.py makemigrations
 python3 manage.py migrate
-if [ $ENV_TYPE = "service" ]
-then
-    python3 manage.py runserver 0.0.0.0:8000
-fi
+python3 manage.py runserver 0.0.0.0:8000
